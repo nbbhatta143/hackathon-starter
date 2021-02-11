2bc6e8626bacf366f602c8965a818e5619f43385
nbbhatta143 <61528359+nbbhatta143@users.noreply.github.com>
2021-02-11 15:18:38 -0500

    added deleted button for each messages on the list

--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -7,10 +7,10 @@ class Messages extends React.Component {
 
     this.state = {
       messages: [],
-      message: '',
+      message: "",
       count: 0,
-      image: ''
-    }
+      image: "",
+    };
   }
 
   componentDidMount() {
@@ -19,49 +19,59 @@ class Messages extends React.Component {
 
   fetchMessages = () => {
     this.props.getMessage(this.props.username).then((res) => {
-      console.log(res.payload)
+      console.log(res.payload);
       this.setState({
         messages: res.payload.messages,
-        count: res.payload.count
-      })
-    })
-  }
+        count: res.payload.count,
+      });
+    });
+  };
 
   newMessageHandler = () => {
     let text = this.state.message;
     this.props.createMessage(text).then(() => {
       this.fetchMessages();
       this.setState({
-        message: ''
-      })
-    })
-  }
+        message: "",
+      });
+    });
+  };
+
+  handleDelete = (e) => {
+    let messageId = e.target.id;
+    this.props.deleteMessage(messageId).then(() => {
+      this.fetchMessages();
+    });
+  };
 
   handleChange = (event) => {
-    let data = {...this.state};
-   
-    data[event.target.name] = event.target.value;   
+    let data = { ...this.state };
+
+    data[event.target.name] = event.target.value;
 
     this.setState(data);
-  }
+  };
 
   render() {
-    let display = (<div>No Messages Found</div>)
+    let display = <div>No Messages Found</div>;
     if (this.state.messages) {
       display = this.state.messages.map((value) => {
         return (
-          <li key={value.id}>{value.text}</li>
-        )
-      })
+          <div>
+            <li key={value.id}>{value.text}</li>
+            <button id={value.id} onClick={this.handleDelete}>
+              Delete
+            </button>
+          </div>
+        );
+      });
     }
 
     return (
       <div className="Messages">
-        <div className="ListMessage">
-          {display}
-        </div>
+        <div className="ListMessage">{display}</div>
         <div className="NewMessage">
-          <input name="message" onChange={this.handleChange} value={this.state.message}/>
+          <input name="message" onChange={this.handleChange} value={this.state.message} />
           <button onClick={this.newMessageHandler}> Send Message </button>
         </div>
       </div>
