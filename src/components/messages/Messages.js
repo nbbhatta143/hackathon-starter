672743a1fb1b819400d212149b22e84c221e4fe0
Rian McCollom <Mail@rian.dev>
2021-02-12 09:47:16 -0600

    Updated delete icons and imported icons from Materialui

--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -1,72 +1,109 @@
+/** @format */
+
 import React from "react";
 import { withAsyncAction } from "../../redux/HOCs";
+import { Button, IconButton, List, ListItem } from "@material-ui/core";
+import DeleteIcon from '@material-ui/icons/Delete';
+
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 
 class Messages extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
+		// this.handleDelete = this.handleDelete.bind(this)
+		this.state = {
+			messages: [],
+			message: "",
+			count: 0,
+			image: "",
+		};
+		
+	}
 
-    this.state = {
-      messages: [],
-      message: '',
-      count: 0,
-      image: ''
-    }
-  }
+	fetchMessages = () => {
+		this.props.getMessage(this.props.username).then((res) => {
+			console.log(res.payload);
+			this.setState({
+				messages: res.payload.messages,
+				count: res.payload.count,
+			});
+		});
+	};
 
-  componentDidMount() {
-    this.fetchMessages();
-  }
+	newMessageHandler = () => {
+		let text = this.state.message;
+		this.props.createMessage(text).then(() => {
+			this.fetchMessages();
+			this.setState({
+				message: "",
+			});
+		});
+	};
 
-  fetchMessages = () => {
-    this.props.getMessage(this.props.username).then((res) => {
-      console.log(res.payload)
-      this.setState({
-        messages: res.payload.messages,
-        count: res.payload.count
-      })
-    })
-  }
+	handleDelete = (messageId) => {
+		// let messageId = e.target.id;
+		this.props.deleteMessage(messageId).then(() => {
+			this.fetchMessages();
+			this.setState({
+				message: "",
+			})
+		});
+	};
 
-  newMessageHandler = () => {
-    let text = this.state.message;
-    this.props.createMessage(text).then(() => {
-      this.fetchMessages();
-      this.setState({
-        message: ''
-      })
-    })
-  }
+	handleChange = (event) => {
+		let data = { ...this.state };
 
-  handleChange = (event) => {
-    let data = {...this.state};
-   
-    data[event.target.name] = event.target.value;   
+		data[event.target.name] = event.target.value;
 
-    this.setState(data);
-  }
+		this.setState(data);
+	};
 
-  render() {
-    let display = (<div>No Messages Found</div>)
-    if (this.state.messages) {
-      display = this.state.messages.map((value) => {
-        return (
-          <li key={value.id}>{value.text}</li>
-        )
-      })
-    }
+	render() {
+		let display = <div>No Messages Found</div>;
 
-    return (
-      <div className="Messages">
-        <div className="ListMessage">
-          {display}
-        </div>
-        <div className="NewMessage">
-          <input name="message" onChange={this.handleChange} value={this.state.message}/>
-          <button onClick={this.newMessageHandler}> Send Message </button>
-        </div>
-      </div>
-    );
-  }
+		if (this.state.messages) {
+			display = this.state.messages.map((value) => {
+				return (
+					<List>
+						<ListItem key={value.id.toString()}>{value.text}
+						<IconButton
+							key={value.id}
+							
+							
+							id={value.id.toString()}
+							// onClick={this.handleDelete}
+							label="delete"
+						> 
+						<DeleteIcon onClick={()=>this.handleDelete(value.id)}></DeleteIcon>
+						</IconButton>
+						</ListItem>
+					</List>
+				);
+			});
+		}
+
+		return (
+			<div className="Messages">
+				<div className="ListMessage">
+					{display}
+					{console.log(display.id)}
+
+					
+				</div>
+
+				<div className="NewMessage">
+					<input name="message" onChange={this.handleChange} value={this.state.message} />
+					<Button component="button" variant="contained" color="primary" onClick={this.newMessageHandler}>
+						{" "}
+						Send Message{" "}
+					</Button>
+
+					{/* <Button onClick={this.fetchMessages}>Show Messages</Button> */}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default withAsyncAction("profile", "all")(Messages);
