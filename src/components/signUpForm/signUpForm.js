110ac010d00018eb59f9ffaf45b468c64e6f3354
Rian McCollom <mail@rian.dev>
2021-02-13 08:37:09 -0600

    Deleting Second render()
    There must have been a merge because this file ended up with two render() sections The first render() is where I added the Material-Ui Component names. Then somehow another render() was inserted that was overiding my edits. I then mistakingling in a hurry did a pull request on the Kenzie project instead of this one. This last edit should be the final one for this project for the hackathon

--- src/components/signUpForm/signUpForm.js
+++ src/components/signUpForm/signUpForm.js
@@ -73,46 +73,6 @@ class SignUpForm extends React.Component {
         );
     }
 
-  render() {
-    const { loading, error } = this.props;
-    return (
-      <div className="signUpForm">
-        <form id="signUp-form" onSubmit={this.handlesignUp}>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            name="username"
-            autoFocus
-            required
-            onChange={this.handleChange}
-            value={this.state.username}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            required
-            onChange={this.handleChange}
-            value={this.state.password}
-          />
-          <label htmlFor="displayName">Dispaly Name</label>
-          <input
-            type="displayName"
-            name="displayName"
-            autoFocus
-            required
-            onChange={this.handleChange}
-          />
-          <button type="submit" disabled={loading}>
-            Sign Up
-          </button>
-        </form>
-        {loading && <Spinner name="circle" color="blue" />}
-        {error && <p style={{ color: "red" }}>{error.message}</p>}
-      </div>
-    );
-  }
-
 }
 
 export default withAsyncAction("auth", "login")(SignUpForm);
