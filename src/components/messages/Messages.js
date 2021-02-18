d715c41e0c0104be7fbd188bcb6cc58f36cc9cd6
nbbhatta143 <61528359+nbbhatta143@users.noreply.github.com>
2021-02-18 10:55:57 -0500

    added dislike button and method to handle it

--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -8,114 +8,124 @@ import "./Message.css";
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 import { ThumbDown } from "@material-ui/icons";
 
-
-
 class Messages extends React.Component {
-	constructor(props) {
-		super(props);
-		// this.handleDelete = this.handleDelete.bind(this)
-		this.state = {
-			messages: [],
-			message: "",
-			count: 0,
-			image: "",
-		};
-		
-	}
-	
-	componentDidMount() {
-        	this.fetchMessages();
-	}
+  constructor(props) {
+    super(props);
+    // this.handleDelete = this.handleDelete.bind(this)
+    this.state = {
+      messages: [],
+      message: "",
+      count: 0,
+      image: "",
+    };
+  }
 
-	fetchMessages = () => {
-		this.props.getMessage(this.props.username).then((res) => {
-			console.log(res.payload);
-			this.setState({
-				messages: res.payload.messages,
-				count: res.payload.count,
-			});
-		});
-	};
+  componentDidMount() {
+    this.fetchMessages();
+  }
 
-	newMessageHandler = () => {
-		let text = this.state.message;
-		this.props.createMessage(text).then(() => {
-			this.fetchMessages();
-			this.setState({
-				message: "",
-			});
-		});
-	};
+  fetchMessages = () => {
+    this.props.getMessage(this.props.username).then((res) => {
+      console.log(res.payload);
+      this.setState({
+        messages: res.payload.messages,
+        count: res.payload.count,
+      });
+    });
+  };
 
-	handleDelete = (messageId) => {
-		// let messageId = e.target.id;
-		this.props.deleteMessage(messageId).then(() => {
-			this.fetchMessages();
-			this.setState({
-				message: "",
-			})
-		});
-	};
+  newMessageHandler = () => {
+    let text = this.state.message;
+    this.props.createMessage(text).then(() => {
+      this.fetchMessages();
+      this.setState({
+        message: "",
+      });
+    });
+  };
 
-	handleChange = (event) => {
-		let data = { ...this.state };
+  handleDelete = (messageId) => {
+    // let messageId = e.target.id;
+    this.props.deleteMessage(messageId).then(() => {
+      this.fetchMessages();
+      this.setState({
+        message: "",
+      });
+    });
+  };
 
-		data[event.target.name] = event.target.value;
+  handleLike = (e) => {
+    this.props.addLike(e.target.id).then(() => {
+      this.fetchMessages();
+    });
+  };
 
-		this.setState(data);
-	};
+  handleChange = (event) => {
+    let data = { ...this.state };
 
-	render() {
-		let display = <div>No Messages Found</div>;
+    data[event.target.name] = event.target.value;
 
-		if (this.state.messages) {
-			display = this.state.messages.map((value) => {
-				return (
-					<List>
-						<ListItem key={value.id.toString()}>{value.text}
-						<IconButton
-							key={value.id}
-							id={value.id.toString()}
-						>
-						<ThumbUpIcon />
-						</IconButton>
-						<IconButton
-							key={value.id}
-							
-							
-							id={value.id.toString()}
-							// onClick={this.handleDelete}
-							label="delete"
-						> 
-						<DeleteIcon onClick={()=>this.handleDelete(value.id)}></DeleteIcon>
-						</IconButton>
-						</ListItem>
-					</List>
-				);
-			});
-		}
+    this.setState(data);
+  };
 
-		return (
-			<div className="Messages">
-				<div className="ListMessage">
-					{display}
-					{console.log(display.id)}
+  render() {
+    let display = <div>No Messages Found</div>;
 
-					
-				</div>
+    if (this.state.messages) {
+      display = this.state.messages.map((value) => {
+        return (
+          <List>
+            <ListItem key={value.id.toString()}>
+              {value.text}
+              <IconButton key={value.id} id={value.id.toString()}>
+                <ThumbUpIcon onClick={this.handleLike} />
+              </IconButton>
+              <IconButton key={value.id} id={value.id.toString()}>
+                <ThumbDown onClick={this.handleLike} />
+              </IconButton>
+              <IconButton
+                key={value.id}
+                id={value.id.toString()}
+                // onClick={this.handleDelete}
+                label="delete"
+              >
+                <DeleteIcon onClick={() => this.handleDelete(value.id)}></DeleteIcon>
+              </IconButton>
+            </ListItem>
+          </List>
+        );
+      });
+    }
 
-				<div className="NewMessage">
-					<Input placeholder="Type Message Here" name="message" onChange={this.handleChange} value={this.state.message} />
-					<Button component="button" variant="contained" color="primary" onClick={this.newMessageHandler}>
-						{" "}
-						Send Message{" "}
-					</Button>
+    return (
+      <div className="Messages">
+        <div className="ListMessage">
+          {display}
+          {console.log(display.id)}
+        </div>
 
-					{/* <Button onClick={this.fetchMessages}>Show Messages</Button> */}
-				</div>
-			</div>
-		);
-	}
+        <div className="NewMessage">
+          <Input
+            placeholder="Type Message Here"
+            name="message"
+            onChange={this.handleChange}
+            value={this.state.message}
+          />
+          <Button
+            component="button"
+            variant="contained"
+            color="primary"
+            onClick={this.newMessageHandler}
+          >
+            {" "}
+            Send Message{" "}
+          </Button>
+
+          {/* <Button onClick={this.fetchMessages}>Show Messages</Button> */}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default withAsyncAction("profile", "all")(Messages);
